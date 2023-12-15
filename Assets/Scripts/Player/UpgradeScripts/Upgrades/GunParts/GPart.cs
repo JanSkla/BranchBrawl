4c3244588ebaf94b83c1db10aafa45ed5e1864a0
JanSkla <jan.skladal11@gmail.com>
2023-12-15 09:23:46 +0100

    GDestiny separation

--- Assets/Scripts/Player/UpgradeScripts/Upgrades/GunParts/GPart.cs
+++ Assets/Scripts/Player/UpgradeScripts/Upgrades/GunParts/GPart.cs
@@ -1,15 +1,6 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public abstract class GPart : MonoBehaviour
 {
     public void Shoot(ShootData shot) { }
-}
-[Serializable]
-public class GDestiny
-{
-    public Transform position;
-    public GPart Part;
 }
