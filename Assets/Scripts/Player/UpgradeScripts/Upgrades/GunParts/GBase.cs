19352eb7904a3e4cbdeb8e8b6afdc17c6206f31a
JanSkla <jan.skladal11@gmail.com>
2024-01-13 12:12:13 +0100

    ChildOnDestiny

--- Assets/Scripts/Player/UpgradeScripts/Upgrades/GunParts/GBase.cs
+++ Assets/Scripts/Player/UpgradeScripts/Upgrades/GunParts/GBase.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
 public class GBase : GPart
@@ -8,5 +9,54 @@ public class GBase : GPart
     public new void Shoot(ShootData shot)
     {
         Destiny.Part.Shoot(shot);
+    }
+
+    private NetworkList<ChildOnDestiny> _childOnDestinies = new();
+
+
+    public override void OnNetworkSpawn()
+    {
+        if (_childOnDestinies != null && _childOnDestinies.Count > 0)
+        {
+            foreach (var child in _childOnDestinies)
+            {
+                NetworkSetParentOnDesinty(child);
+            }
+        }
+
+        _childOnDestinies.OnListChanged += OnChODChange;
+    }
+
+    private void OnChODChange(NetworkListEvent<ChildOnDestiny> changeEvent)
+    {
+        if (changeEvent.Type == NetworkListEvent<ChildOnDestiny>.EventType.Add)
+        {
+            ChildOnDestiny newChOD = changeEvent.Value;
+
+            NetworkSetParentOnDesinty(newChOD);
+        }
+    }
+
+    private void NetworkSetParentOnDesinty(ChildOnDestiny ChOD)
+    {
+        NetworkObject childNwObject = NetworkManager.Singleton.SpawnManager.SpawnedObjects[ChOD.ChildNwId];
+        GDestiny destiny = Destiny;
+
+        if (childNwObject == null) Debug.LogError("There is no destiny with mentioned index");
+        if (destiny == null) Debug.LogError("There is no destiny with mentioned index");
+
+        childNwObject.AutoObjectParentSync = false;
+        childNwObject.transform.SetParent(destiny.Position, false);
+    }
+
+    public void NetworkAddParentOnDestiny(int destinyIndex, ulong childNwId)
+    {
+        ChildOnDestiny chod = new()
+        {
+            DestinyIndex = destinyIndex,
+            ChildNwId = childNwId
+        };
+
+        _childOnDestinies.Add(chod);
     }
 }
--- Assets/Scripts/Player/UpgradeScripts/Upgrades/GunParts/GUpgrade.cs
+++ Assets/Scripts/Player/UpgradeScripts/Upgrades/GunParts/GUpgrade.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
 public abstract class GUpgrade : GPart
@@ -11,6 +13,52 @@ public abstract class GUpgrade : GPart
     {
         get { return _destiny; }
         set { _destiny = value; }
+    }
+
+    private NetworkList<ChildOnDestiny> _childOnDestinies = new();
+
+
+    public override void OnNetworkSpawn()
+    {
+        if(_childOnDestinies != null && _childOnDestinies.Count > 0)
+        {
+            foreach (var child in _childOnDestinies)
+            {
+                NetworkSetParentOnDesinty(child);
+            }
+        }
+
+        _childOnDestinies.OnListChanged += OnChODChange;
+    }
+
+    private void OnChODChange(NetworkListEvent<ChildOnDestiny> changeEvent)
+    {
+        if(changeEvent.Type == NetworkListEvent<ChildOnDestiny>.EventType.Add)
+        {
+            ChildOnDestiny newChOD = changeEvent.Value;
+
+            NetworkSetParentOnDesinty(newChOD);
+        }
+    }
+
+    private void NetworkSetParentOnDesinty(ChildOnDestiny ChOD)
+    {
+        NetworkObject childNwObject = NetworkManager.Singleton.SpawnManager.SpawnedObjects[ChOD.ChildNwId];
+        GDestiny destiny = _destiny[ChOD.DestinyIndex];
+
+        if (childNwObject == null) Debug.LogError("There is no destiny with mentioned index");
+        if (destiny == null) Debug.LogError("There is no destiny with mentioned index");
+
+        childNwObject.AutoObjectParentSync = false;
+        childNwObject.transform.SetParent(destiny.Position, false);
+    }
+
+    public void NetworkAddParentOnDestiny(int destinyIndex, ulong childNwId)
+    {
+        ChildOnDestiny chod = new() { DestinyIndex = destinyIndex,
+            ChildNwId = childNwId };
+
+        _childOnDestinies.Add(chod);
     }
 
     public void ReplacePart(UpgradeWithPart guPrefab, bool isNetwork = false)
