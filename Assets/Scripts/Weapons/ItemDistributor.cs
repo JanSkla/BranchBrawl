81f65bbd2109aa9fd570172b137215a5e4faf336
JanSkla <jan.skladal11@gmail.com>
2024-03-02 16:12:27 +0100

    item distribution on terrain

---
+++ Assets/Scripts/Weapons/ItemDistributor.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class ItemDistributor : MonoBehaviour
+{
+    [SerializeField]
+    GameObject _itemToSpawn;
+    [SerializeField]
+    Texture2D _itemSpawnHeatmap;
+    [SerializeField]
+    Transform _bottomLeft;
+    [SerializeField]
+    Transform _topRight;
+
+    private void Start()
+    {
+        SpawnItems();
+    }
+    public void SpawnItems()
+    {
+        Vector2 bottomLeft = new(_bottomLeft.position.x, _bottomLeft.position.z);
+        Vector2 topRight = new(_topRight.position.x, _topRight.position.z);
+
+        var positions = FastPoissonDiskSampling.Sampling(bottomLeft, topRight, 10);
+
+        Vector2 widthXheight = topRight - bottomLeft;
+
+        foreach (var pos in positions)
+        {
+            //Spawn and distribute along texture
+            Vector2 pixel = (pos-bottomLeft) / widthXheight * _itemSpawnHeatmap.width;
+            Color rate = _itemSpawnHeatmap.GetPixel((int)pixel.x, (int)pixel.y);
+
+            if(rate.r >= Random.Range(0f, 1f))
+            {
+                //Drop to ground lvl
+                Physics.Raycast(new Vector3(pos.x, transform.position.y, pos.y), Vector3.down, out RaycastHit hit, 500, LayerMask.GetMask("Terrain"));
+                var obj = Instantiate(_itemToSpawn);
+                obj.transform.position = new(hit.point.x, hit.point.y + 1, hit.point.z);
+
+                obj.GetComponent<NetworkObject>().Spawn();
+            }
+        }
+    }
+}
